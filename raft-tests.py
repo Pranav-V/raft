d32f5ee27af9a261a044e334b4cd216fee0f717a
Ritesh Thakur <ritz.thakur@gmail.com>
2024-12-12 19:19:00 -0600

    init version of testing suite

--- raft-tests.py
+++ raft-tests.py
@@ -1,13 +1,115 @@
+# import grpc
+# import raft_pb2
+# import raft_pb2_grpc
+# import time
+
+# channel = grpc.insecure_channel("localhost:8001")
+# stub = raft_pb2_grpc.FrontEndStub(channel)
+
+# ag = raft_pb2.IntegerArg(arg=2)
+# stub.StartRaft(ag)
+# time.sleep(3)
+# ag = raft_pb2.KeyValue(key="hello", value="there", ClientId=1, RequestId=2)
+# stub.Put(ag)
+
+# testing
+import unittest
+
+# networking
+import concurrent.futures
+
+# grpc + protobuf
 import grpc
 import raft_pb2
 import raft_pb2_grpc
-import time
 
-channel = grpc.insecure_channel("localhost:8001")
-stub = raft_pb2_grpc.FrontEndStub(channel)
+# overloaded services
+from frontend import FrontEnd
+from keyvaluestore import KeyValueStore, RaftServer
 
-ag = raft_pb2.IntegerArg(arg=2)
-stub.StartRaft(ag)
-time.sleep(3)
-ag = raft_pb2.KeyValue(key="hello", value="there", ClientId=1, RequestId=2)
-stub.Put(ag)
+TEST_CLIENT_ID = 31415
+test_request_id = 1
+
+def get_requestid():
+    old_req_id = test_request_id
+    test_request_id += 1
+    return old_req_id
+    
+
+class TestRaftFunctionality(unittest.TestCase):
+
+    frontend = None
+
+    # frontend functionality
+
+    def test_frontend_init(self):
+        frontend_server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=10))
+        frontend_servicer = FrontEnd()
+        raft_pb2_grpc.add_FrontEndServicer_to_server(frontend_servicer, frontend_server)
+        frontend_server.add_insecure_port(f"[::]:{FrontEnd.SERVER_PORT}")
+
+        frontend_server.start()
+        frontend_servicer.log_msg("[raft-tests.py] Started Fronted")
+        frontend_server.wait_for_termination()
+
+    def test_frontend_startraft(self):
+        channel = grpc.insecure_channel("localhost:8001")
+        frontend = raft_pb2_grpc.FrontEndStub(channel)
+
+        ag = raft_pb2.IntegerArg(arg=10)
+        frontend.StartRaft(ag)
+
+    def test_frontend_put_get(self):
+
+        pass
+
+        # test get (key not present)
+        request = raft_pb2.GetKey(
+            key = 'test_key',
+            ClientId = TEST_CLIENT_ID,
+            RequestId = get_requestid()
+        )
+        target = ''
+        self.frontend.Get(
+            request = request,
+            target = target
+        )
+
+        # test put
+        request = raft_pb2.KeyValue(
+            key = 'test_key',
+            value = 'test_val',
+            ClientId = TEST_CLIENT_ID,
+            RequestId = get_requestid()
+        )
+        target = ''
+        self.frontend.Put(
+            request = request,
+            target = target
+        )
+
+        # test get (key present)
+        request = raft_pb2.GetKey(
+            key = 'test_key',
+            ClientId = TEST_CLIENT_ID,
+            RequestId = get_requestid()
+        )
+        target = ''
+        self.frontend.Put(
+            request = request,
+            target = target
+        )
+
+    def test_frontend_replace(self):
+        # Your test code for method3
+        pass
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(TestRaftFunctionality('test_frontend_init'))
+    suite.addTest(TestRaftFunctionality('test_frontend_startraft'))
+    suite.addTest(TestRaftFunctionality('test_frontend_put_get'))
+    suite.addTest(TestRaftFunctionality('test_frontend_replace'))
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
